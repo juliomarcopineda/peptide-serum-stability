ad8fd02315974448ea6709a29ab821667b70622c
juliomarcopineda <juliomarcopineda@gmail.com>
2018-03-01 23:09:44 -0500

    test resource class path

---
+++ peptideSerumStability/src/com/github/juliomarcopineda/ResourceTest.java
@@ -0,0 +1,27 @@
+package com.github.juliomarcopineda;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ResourceTest {
+
+	public static void main(String[] args) {
+		InputStream test = ResourceTest.class.getResourceAsStream("/test.txt");
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(test));
+		
+		String line;
+		try {
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
