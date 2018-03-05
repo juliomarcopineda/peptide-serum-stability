9201632473c5a7ac823a335844ca4e8ed9b63c61
juliomarcopineda <juliomarcopineda@gmail.com>
2018-03-04 20:02:28 -0500

    measure molecular weight of all fragments

--- peptideSerumStability/src/com/github/juliomarcopineda/FragmentAnalyzer.java
+++ peptideSerumStability/src/com/github/juliomarcopineda/FragmentAnalyzer.java
@@ -1,14 +1,12 @@
 package com.github.juliomarcopineda;
 
-import java.util.AbstractMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import com.github.juliomarcopineda.peptide.Peptide;
 import com.github.juliomarcopineda.peptide.PeptideType;
@@ -18,11 +16,20 @@ public class FragmentAnalyzer {
 	private Map<Character, Double> weights;
 	
 	private List<List<Integer>> fragments;
+	private Map<String, Double> fragmentWeights;
 	
 	public FragmentAnalyzer(Peptide peptide) {
 		this.peptide = peptide;
 		this.weights = createAminoAcideWeightMap();
 		this.fragments = new ArrayList<>();
+	}
+	
+	public Map<String, Double> getFragmentWeights() {
+		return fragmentWeights;
+	}
+	
+	public void setFragmentWeights(Map<String, Double> fragmentWeights) {
+		this.fragmentWeights = fragmentWeights;
 	}
 	
 	public List<List<Integer>> getFragments() {
@@ -34,15 +41,96 @@ public class FragmentAnalyzer {
 	}
 	
 	/**
+	 * Populates the map of fragment (represented by String) and its theoretical molecular weight.
+	 * 
+	 * @return
+	 */
+	public FragmentAnalyzer measureAllFragmentWeights() {
+		Map<String, Double> fragmentWeights = new HashMap<>();
+		
+		PeptideType type = this.peptide.getType();
+		
+		for (List<Integer> fragmentIndex : this.fragments) {
+			String fragment = getPeptideStringRepresentation(fragmentIndex, type);
+			double weight = calculateFragmentWeight(fragment);
+			
+			fragmentWeights.put(fragment, weight);
+		}
+		
+		this.fragmentWeights = fragmentWeights;
+		
+		return this;
+	}
+	
+	/**
+	 * Given the peptide represented by its index and the peptide type, return the String representation 
+	 * of the peptide.
+	 * 
+	 * @param peptideIndex
+	 * @param type
+	 * @return
+	 */
+	private String getPeptideStringRepresentation(List<Integer> peptideIndex, PeptideType type) {
+		String peptideSequence = this.peptide.getSequence();
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for (int index : peptideIndex) {
+			if (index < peptideSequence.length()) {
+				stringBuilder.append(peptideSequence.charAt(index));
+			}
+			else {
+				switch (type) {
+					case DFBP:
+						stringBuilder.append("2");
+						break;
+					case DISULFIDE:
+						stringBuilder.append("S");
+						break;
+					case LINEAR:
+						break;
+					case AMIDE:
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		
+		return stringBuilder.toString();
+	}
+	
+	/**
+	 * Given a fragment with its String representation, calculates its theoretical molecular weight.
+	 * 
+	 * @param fragment
+	 * @return
+	 */
+	private double calculateFragmentWeight(String fragment) {
+		double sum = 0;
+		
+		for (int i = 0; i < fragment.length(); i++) {
+			char symbol = fragment.charAt(i);
+			sum += this.weights.get(symbol);
+		}
+		
+		sum = sum - (18.0 * (fragment.length() - 1));
+		
+		return sum;
+	}
+	
+	/**
 	 * Initiates the process of finding all the possible fragments of the peptide.
 	 */
-	public void findAllFragments() {
+	public FragmentAnalyzer findAllFragments() {
 		Map<Integer, List<Integer>> graph = this.peptide.getGraph();
 		for (Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {
 			int start = entry.getKey();
 			
 			walkGraph(start, graph);
 		}
+		
+		return this;
 	}
 	
 	/**
@@ -115,167 +203,33 @@ public class FragmentAnalyzer {
 		}
 	}
 	
-	private double fragmentWeight(List<Integer> fragmentIndex) {
-		double sum = 0;
-		
-		for (int index : fragmentIndex) {
-			
-			char symbol = this.peptide.getSequence()
-				.charAt(index);
-			sum += this.weights.get(symbol);
-		}
-		
-		sum = sum - (18.0 * (fragmentIndex.size() - 1));
-		
-		return sum;
-	}
-	
+	/**
+	 * Populate the amino acid weight map using the weights.csv file.
+	 * 
+	 * @return
+	 */
 	private Map<Character, Double> createAminoAcideWeightMap() {
 		Map<Character, Double> aminoAcidWeight = new HashMap<>();
-		aminoAcidWeight.put('I', 131.17);
-		aminoAcidWeight.put('L', 131.17);
-		aminoAcidWeight.put('K', 146.19);
-		aminoAcidWeight.put('M', 149.21);
-		aminoAcidWeight.put('F', 165.19);
-		aminoAcidWeight.put('T', 119.12);
-		aminoAcidWeight.put('W', 204.23);
-		aminoAcidWeight.put('V', 117.15);
-		aminoAcidWeight.put('R', 174.20);
-		aminoAcidWeight.put('H', 115.16);
-		aminoAcidWeight.put('A', 89.09);
-		aminoAcidWeight.put('N', 132.12);
-		aminoAcidWeight.put('D', 133.10);
-		aminoAcidWeight.put('C', 121.16);
-		aminoAcidWeight.put('E', 147.13);
-		aminoAcidWeight.put('Q', 146.15);
-		aminoAcidWeight.put('G', 75.07);
-		aminoAcidWeight.put('P', 115.13);
-		aminoAcidWeight.put('S', 105.09);
-		aminoAcidWeight.put('Y', 181.19);
-		aminoAcidWeight.put('B', 224.31);
-		aminoAcidWeight.put('1', 43.04); // Acetylated N-terminus
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("../weights.csv"));
+			String line = reader.readLine(); // ignore header
+			while ((line = reader.readLine()) != null) {
+				
+				String[] split = line.split(",");
+				char symbol = split[0].charAt(0);
+				double weight = Double.parseDouble(split[1]);
+				
+				aminoAcidWeight.put(symbol, weight);
+			}
+			
+			reader.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		return aminoAcidWeight;
 	}
 	
-	public static void main(String[] args) {
-		String peptideSequence = "CGYEQDPWGVRYWYGCKKKKB";
-		List<Integer> connections = Arrays.asList(0, 15);
-		PeptideType type = PeptideType.DFBP;
-		Map<Integer, List<Integer>> graph = createGraphStructure(peptideSequence, connections, type);
-		
-		Peptide peptide = new Peptide();
-		peptide.setSequence(peptideSequence);
-		peptide.setConnections(connections);
-		peptide.setType(type);
-		peptide.setGraph(graph);
-		
-		FragmentAnalyzer analyzer = new FragmentAnalyzer(peptide);
-		
-		graph.entrySet()
-			.forEach(e -> {
-				int source = e.getKey();
-				List<Integer> targets = e.getValue();
-				
-				System.out.println(source + " -> " + targets);
-			});
-		
-		System.out.println();
-		
-		analyzer.findAllFragments();
-		List<List<Integer>> fragments = analyzer.getFragments();
-		fragments.stream()
-			.forEach(System.out::println);
-		System.out.println();
-		System.out.println(fragments.size());
-	}
-	
-	private static Map<Integer, List<Integer>> createGraphStructure(String peptideSequence, List<Integer> connections, PeptideType type) {
-		
-		// Create the graph structure of the peptide base sequence
-		Map<Integer, List<Integer>> graph = IntStream.range(0, peptideSequence.length() - 1)
-			.mapToObj(source -> {
-				int target = source + 1;
-				
-				List<Integer> targets = new ArrayList<>();
-				targets.add(target);
-				
-				return new AbstractMap.SimpleEntry<Integer, List<Integer>>(source, targets);
-			})
-			.sorted(Map.Entry.comparingByKey())
-			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (collision1, collision2) -> collision1, LinkedHashMap::new));
-		
-		// Add any cyclic connections if connections is not empty
-		if (!connections.isEmpty()) {
-			switch (type) {
-				case AMIDE:
-					for (int i = 0; i < connections.size(); i++) {
-						if (i % 2 == 0) {
-							graph.get(connections.get(i))
-								.add(connections.get(i + 1));
-						}
-						else {
-							graph.get(connections.get(i))
-								.add(connections.get(i - 1));
-						}
-					}
-					
-					break;
-				
-				case DFBP:
-					int dfbpIndex = peptideSequence.length();
-					
-					for (int connection : connections) {
-						// Add connections from DFBP
-						if (!graph.containsKey(dfbpIndex)) {
-							List<Integer> targets = new ArrayList<>();
-							targets.add(connection);
-							
-							graph.put(dfbpIndex, targets);
-						}
-						else {
-							graph.get(dfbpIndex)
-								.add(connection);
-						}
-						
-						// Add connections to DFBP
-						graph.get(connection)
-							.add(dfbpIndex);
-					}
-					
-					break;
-				case DISULFIDE:
-					// Create disulfide bridge
-					int s1Index = peptideSequence.length();
-					int s2Index = s1Index + 1;
-					
-					List<Integer> s1ToS2 = new ArrayList<>();
-					s1ToS2.add(s2Index);
-					
-					List<Integer> s2ToS1 = new ArrayList<>();
-					s2ToS1.add(s1Index);
-					
-					graph.put(s1Index, s1ToS2);
-					graph.put(s2Index, s2ToS1);
-					
-					// Add connections from peptide base
-					graph.get(connections.get(0))
-						.add(s1Index);
-					graph.get(connections.get(1))
-						.add(s2Index);
-					
-					// Add connections from disulfide bridge
-					graph.get(s1Index)
-						.add(connections.get(0));
-					graph.get(s2Index)
-						.add(connections.get(1));
-					
-					break;
-				case LINEAR:
-					break;
-			}
-		}
-		
-		return graph;
-	}
 }
---
+++ peptideSerumStability/src/com/github/juliomarcopineda/experiments/Test.java
@@ -0,0 +1,127 @@
+package com.github.juliomarcopineda.experiments;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.github.juliomarcopineda.FragmentAnalyzer;
+import com.github.juliomarcopineda.peptide.Peptide;
+import com.github.juliomarcopineda.peptide.PeptideType;
+
+public class Test {
+	public static void main(String[] args) {
+		String peptideSequence = "CGYEQDPWGVRYWYGCKKKKB";
+		List<Integer> connections = Arrays.asList(0, 15);
+		PeptideType type = PeptideType.DFBP;
+		Map<Integer, List<Integer>> graph = createGraphStructure(peptideSequence, connections, type);
+		
+		Peptide peptide = new Peptide();
+		peptide.setSequence(peptideSequence);
+		peptide.setConnections(connections);
+		peptide.setType(type);
+		peptide.setGraph(graph);
+		
+		FragmentAnalyzer analyzer = new FragmentAnalyzer(peptide);
+		analyzer.findAllFragments()
+			.measureAllFragmentWeights();
+		
+		Map<String, Double> fragmentWeights = analyzer.getFragmentWeights();
+		fragmentWeights.entrySet()
+			.stream()
+			.forEach(System.out::println);
+	}
+	
+	private static Map<Integer, List<Integer>> createGraphStructure(String peptideSequence, List<Integer> connections, PeptideType type) {
+		
+		// Create the graph structure of the peptide base sequence
+		Map<Integer, List<Integer>> graph = IntStream.range(0, peptideSequence.length() - 1)
+			.mapToObj(source -> {
+				int target = source + 1;
+				
+				List<Integer> targets = new ArrayList<>();
+				targets.add(target);
+				
+				return new AbstractMap.SimpleEntry<Integer, List<Integer>>(source, targets);
+			})
+			.sorted(Map.Entry.comparingByKey())
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (collision1, collision2) -> collision1, LinkedHashMap::new));
+		
+		// Add any cyclic connections if connections is not empty
+		if (!connections.isEmpty()) {
+			switch (type) {
+				case AMIDE:
+					for (int i = 0; i < connections.size(); i++) {
+						if (i % 2 == 0) {
+							graph.get(connections.get(i))
+								.add(connections.get(i + 1));
+						}
+						else {
+							graph.get(connections.get(i))
+								.add(connections.get(i - 1));
+						}
+					}
+					
+					break;
+				
+				case DFBP:
+					int dfbpIndex = peptideSequence.length();
+					
+					for (int connection : connections) {
+						// Add connections from DFBP
+						if (!graph.containsKey(dfbpIndex)) {
+							List<Integer> targets = new ArrayList<>();
+							targets.add(connection);
+							
+							graph.put(dfbpIndex, targets);
+						}
+						else {
+							graph.get(dfbpIndex)
+								.add(connection);
+						}
+						
+						// Add connections to DFBP
+						graph.get(connection)
+							.add(dfbpIndex);
+					}
+					
+					break;
+				case DISULFIDE:
+					// Create disulfide bridge
+					int s1Index = peptideSequence.length();
+					int s2Index = s1Index + 1;
+					
+					List<Integer> s1ToS2 = new ArrayList<>();
+					s1ToS2.add(s2Index);
+					
+					List<Integer> s2ToS1 = new ArrayList<>();
+					s2ToS1.add(s1Index);
+					
+					graph.put(s1Index, s1ToS2);
+					graph.put(s2Index, s2ToS1);
+					
+					// Add connections from peptide base
+					graph.get(connections.get(0))
+						.add(s1Index);
+					graph.get(connections.get(1))
+						.add(s2Index);
+					
+					// Add connections from disulfide bridge
+					graph.get(s1Index)
+						.add(connections.get(0));
+					graph.get(s2Index)
+						.add(connections.get(1));
+					
+					break;
+				case LINEAR:
+					break;
+			}
+		}
+		
+		return graph;
+	}
+}
