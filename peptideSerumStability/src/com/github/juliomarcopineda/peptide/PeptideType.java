fcc67d3c317c3c4b63536f665bc54e04c1c70989
juliomarcopineda <juliomarcopineda@gmail.com>
2018-04-16 21:09:07 -0400

    comments and delete test class

--- peptideSerumStability/src/com/github/juliomarcopineda/peptide/PeptideType.java
+++ peptideSerumStability/src/com/github/juliomarcopineda/peptide/PeptideType.java
@@ -1,5 +1,11 @@
 package com.github.juliomarcopineda.peptide;
 
+/**
+ * The following are the different options of peptide types that can be used for peptide-serum-stability.
+ * 
+ * @author Julio Pineda
+ *
+ */
 public enum PeptideType {
 	AMIDE,
 	DISULFIDE,
